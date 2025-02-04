5c2f472a17de8561d9c8635a49d3b602df87e51b
Edgars Cirulis <edgarsciruliss@gmail.com>
2025-02-04 17:31:01 +0200

    Update to S918BXXS7CXL2
    Signed-off-by: Edgars Cirulis <edgarsciruliss@gmail.com>

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #1. target config
-BUILD_TARGET=dm3q_eur_openx
+BUILD_TARGET=$1
 export MODEL=$(echo $BUILD_TARGET | cut -d'_' -f1)
 export PROJECT_NAME=${MODEL}
 export REGION=$(echo $BUILD_TARGET | cut -d'_' -f2)
 export CARRIER=$(echo $BUILD_TARGET | cut -d'_' -f3)
-export TARGET_BUILD_VARIANT=user
-
+export TARGET_BUILD_VARIANT=$2
+		
+		
 #2. sm8550 common config
-CHIPSET_NAME=kalama
+CHIPSET_NAME=$3
 
 export ANDROID_BUILD_TOP=$(pwd)
 export TARGET_PRODUCT=gki
@@ -29,16 +30,15 @@ export KBUILD_EXTRA_SYMBOLS="${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mmr
 # for Audio(techpack) driver build
 export MODNAME=audio_dlkm
 
-export KBUILD_EXT_MODULES="${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/msm_ext_display \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/sync_fence \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/hw_fence \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mmrm-driver \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/securemsm-kernel \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/display-drivers/msm \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/audio-kernel \
-  ${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/camera-kernel \
+export KBUILD_EXT_MODULES="../vendor/qcom/opensource/mm-drivers/msm_ext_display \
+  ../vendor/qcom/opensource/mm-drivers/sync_fence \
+  ../vendor/qcom/opensource/mm-drivers/hw_fence \
+  ../vendor/qcom/opensource/mmrm-driver \
+  ../vendor/qcom/opensource/securemsm-kernel \
+  ../vendor/qcom/opensource/display-drivers/msm \
+  ../vendor/qcom/opensource/audio-kernel \
+  ../vendor/qcom/opensource/camera-kernel \
   "
   
 #3. build kernel
-cd kernel_platform
-RECOMPILE_KERNEL=1 ./build/android/prepare_vendor.sh ${CHIPSET_NAME} ${TARGET_PRODUCT}
+RECOMPILE_KERNEL=1 ./kernel_platform/build/android/prepare_vendor.sh sec ${TARGET_PRODUCT}
